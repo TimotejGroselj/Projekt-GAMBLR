25b910eeb5dd0941a713656bf2e12fa610ff402d
Jauka <jaka.perbil@gmail.com>
2025-03-30 23:30:55 +0200

    ema izgleda kt ema

--- EMA UUUUUU.py
+++ EMA UUUUUU.py
@@ -2,31 +2,20 @@ import requests
 from start import bitcoin_price,graf_data
 import datetime
 
-
-class EMA:
-    #from start import bitcoin_price, graf_data
-    import datetime
-
-    def __init__(self,time_period,start="0"):
-        self.N = time_period
-        self.date = start.split()[0]
-
-    def get_EMA_today(self):
-        alpha = 2 / (self.N + 1)
-        print(bitcoin_price)
-        if self.date in [i[0] for i in bitcoin_price['prices']] and self.date != "0":
-            raise "Invalid Date"
-        #for i in range()
+def get_EMA_today(N,smoothing=2):
+    """
+    :param N:za kok dni te zanima
+    :param smoothing: basicly konstanta
+    :return: vrne vrenost EMA v N tem dnevu
+    """
+    alpha = smoothing / (N + 1)
+    EMAp = 60000 #to se se nastav po potrebi
+    for i in range(N):
+        date,value = bitcoin_price['prices'][i]
+        EMAt = alpha*value + (1-alpha)*EMAp
+        EMAp=EMAt
+    return EMAt,value
 
 
-"""
-short: N=9,12,20
-mid: N=26,50
-longshlong: N=100,200
-"""
-N = 26
-alpha = 2/(N+1)
-EMAt =1
-print(bitcoin_price)
-#bull = EMA(26,"2025-03-30")
-#print(bull.get_EMA_today())
+print(get_EMA_today(50))
+
