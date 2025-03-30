f5678b4e01830421845456109e46f3b22dc444af
Timotej <you@example.com>
2025-03-30 20:10:12 +0200

    graf actually dela

--- graf.py
+++ graf.py
@@ -1,7 +1,11 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import datetime
 from start import bitcoin_price,graf_data
 
+table=bitcoin_price["prices"]
+x=[datetime.datetime.fromtimestamp(int(str(el[0])[:-3])) for el in table]
+y=[el[1] for el in table]
 
-
-
+fig=plt.figure()
+plt.plot(x,y)
+plt.show()
