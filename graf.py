0af03a22380cd42900dcfda3c2cffb3ff3220825
Timotej <you@example.com>
2025-03-30 20:30:24 +0200

    upgrade

--- graf.py
+++ graf.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 import datetime
 from start import bitcoin_price,graf_data
 
-table=bitcoin_price["prices"]
-x=[datetime.datetime.fromtimestamp(int(str(el[0])[:-3])) for el in table]
-y=[el[1] for el in table]
+table_bit=bitcoin_price["prices"]
+x1=[datetime.datetime.fromtimestamp(int(str(el[0])[:-3])) for el in table_bit]
+y=[el[1] for el in table_bit]
 
 fig=plt.figure()
-plt.plot(x,y)
+plt.plot(x1,y,label="price graph in USD")
+plt.xlabel("date")
+plt.ylabel("price in USD")
+plt.legend()
 plt.show()
