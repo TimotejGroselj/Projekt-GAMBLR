9337257743b01d24d79480979ac81acde04b2fb5
Timotej <timotej.groselj1@gmail.com>
2025-03-31 09:58:31 +0200

    start ne dela prou nuca še datetime nrdit lepo v str

--- graf.py
+++ graf.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 
-from start import bitcoin_price,graf_data
+from start import coin_prices,graf_data
 
-table_bit=bitcoin_price["prices"]
-x1=[el[0] for el in table_bit]
-y=[el[1] for el in table_bit]
+table_bit=coin_prices["prices"]
+x=[]
+for coin in coin_prices:
+    x.append([el[0] for el in coin_prices["prices"][:-1]])
+    y=[el[1] for el in coin_prices["prices"][:-1]]
 
 fig=plt.figure()
-plt.plot(x1,y,label="price graph in USD")
-plt.xlabel("date")
-plt.ylabel("price in USD")
-plt.legend()
-plt.show()
+for coinx in x:
+    plt.plot(coinx,y,label="price graph in USD")
+    plt.xlabel("date")
+    plt.ylabel("price in USD")
+    plt.legend()
+    plt.show()
--- start.py
+++ start.py
@@ -1,9 +1,35 @@
 import requests
 import datetime
-url="https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=364"
-url2="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=price_change_percentage_24h_desc&per_page=10&page=1&sparkline=false"
-bitcoin_price=requests.get(url).json()
-graf_data=requests.get(url2).json()
-for key in bitcoin_price:
-    bitcoin_price[key]=[[datetime.datetime.fromtimestamp(int(str(el[0])[:-3])),el[1]] for el in bitcoin_price[key]]
+import time
+
+url1="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=price_change_percentage_24h_desc&per_page=10&page=1&sparkline=false"
+response=requests.get(url1)
+if response.status_code!=200:
+    print("napaka preveč")
+graf_data=response.json()
+coin_prices=dict()
+
+
+while len(graf_data)!=len(coin_prices):
+    for el in graf_data:
+        #print(el["id"])
+        if el['id'] not in coin_prices:
+            url=f"https://api.coingecko.com/api/v3/coins/{el['id']}/market_chart?vs_currency=usd&days=364"
+            response=requests.get(url)
+            print(response)
+            if response.status_code==200:
+                coin_prices[el["id"]]=response.json()
+                with open(f"{el['id']}","w") as data:
+                    for key in coin_prices[el["id"]]:
+                        data.write(f"{key}:\n")
+                        for par in coin_prices[el["id"]][key]:
+                            par[0]=datetime.datetime.fromtimestamp(par[0]/1000)
+                            data.write(f"\t{par}\n")
+
+                            
+            else:
+                time.sleep(5)
+
+
+
 
