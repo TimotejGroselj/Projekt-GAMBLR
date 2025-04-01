ea577c5d033609ccde69e724be8206d4084bbc5a
Timotej <you@example.com>
2025-04-01 20:36:06 +0200

    mal frama ce bos mel voljo si oglej pa sam kj dodej

--- testalgoritem.py
+++ testalgoritem.py
@@ -6,11 +6,19 @@
 # dan "denar"
 # procenti za use te vrednosti k jih bo treba zbruteforcat da bojo prou
 ###
+import datetime
+def bot(podatki,money,kdm,kdv,kdp,km):
+    """
+    money=mone
+    kdm=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva delta eme
+    kdv=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva delta cene
+    kdp=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva razdalja od max in min
+    km=koef v % (vrednosti od 0 do 1) kolk mone dava notr/vn glede na izračunano številko
 
-def bot(podatki,money,kdm,kdv,kdp,kdmo):
+    """
     investicije=dict()
     #ključ:datum
-    #vrednost:vložen denar (-float)/pobran denar ven(float)
+    #vrednost:vložen denar (-float)/pobran denar ven(float) sprot updatas money
     #če na dan nč ne nrdiš ni elementa
     for el in podatki:
         #podatki bojo neki nek iterator v kirm bo datum,ema,cena za cel dataset do datuma
@@ -19,6 +27,11 @@ def bot(podatki,money,kdm,kdv,kdp,kdmo):
         price=el["price"]
         past_low=min(price)
         past_max=max(price)
+        dm="delta eme"
+        dv="delta vrednosti"
+        dp="razdalja od min in max iz preteklosti"
+        
+        
         
     
     
