bce8f8cab64a51d4c70b1a69432eea3d75cdd4d7
Timotej <you@example.com>
2025-04-03 20:27:59 +0200

    graf sinenigans

--- graf.py
+++ graf.py
@@ -1,43 +1,46 @@
 import matplotlib.pyplot as plt
-import pickle
 import numpy as np
-from classdat import coin
 from math import floor
 
-
+import pickle
+from classdat import coin
 with open("data.bin","rb") as data:
     coin_prices=pickle.load(data)
     
-    fig,axs=plt.subplots(2,2,figsize=[19.2,10.8])
+    fig=plt.figure(figsize=[19.2,10.8])
 
     for obj_coin in coin_prices.values():
         d=len(obj_coin.getprices())
         break
         
     for obj_coin in coin_prices.values():
-        axs[0][0].semilogy(list(obj_coin.getprices().keys()),list(obj_coin.getprices().values()),label=obj_coin.getname(),linewidth =2)
-        axs[0][0].set_xticks([list(obj_coin.getprices().keys())[i] for i in range(0,d,floor(d/6))])
-    axs[0][0].set_xlabel("date")
-    axs[0][0].set_ylabel("price in USD")
-    axs[0][0].set_title("Prices")
-    axs[0][0].legend()
-
-
+        plt.semilogy(list(obj_coin.getprices().keys()),list(obj_coin.getprices().values()),label=obj_coin.getname(),linewidth =2)
+        plt.xticks([list(obj_coin.getprices().keys())[i] for i in range(0,d,floor(d/12))])
+        plt.title("Prices")
+    plt.xlabel("date")
+    plt.ylabel("price in USD")
+    plt.legend()
+    fig.savefig("Prices.pdf",bbox_inches="tight")
+    
+    fig=plt.figure(figsize=[19.2,10.8])
     for obj_coin in coin_prices.values():
-        axs[0][1].semilogy(list(obj_coin.getmarket_caps().keys()),list(obj_coin.getmarket_caps().values()),label=obj_coin.getname(),linewidth =2)
-        axs[0][1].set_xticks([list(obj_coin.getmarket_caps().keys())[i] for i in range(0,d,floor(d/6))])
-    axs[0][1].set_xlabel("date")
-    axs[0][1].set_ylabel("market cap in USD")
-    axs[0][1].set_title("Market caps")
-    axs[0][1].legend()
+        plt.semilogy(list(obj_coin.getmarket_caps().keys()),list(obj_coin.getmarket_caps().values()),label=obj_coin.getname(),linewidth =2)
+        plt.xticks([list(obj_coin.getmarket_caps().keys())[i] for i in range(0,d,floor(d/12))])
+    plt.xlabel("date")
+    plt.ylabel("market cap in USD")
+    plt.title("Market caps")
+    plt.legend()
+    fig.savefig("Market_caps.pdf",bbox_inches="tight")
     
     for obj_coin in coin_prices.values():
-        axs[1][0].semilogy(list(obj_coin.gettotal_volumes().keys()),list(obj_coin.gettotal_volumes().values()),label=obj_coin.getname())
-        axs[1][0].set_xticks([list(obj_coin.gettotal_volumes().keys())[i] for i in range(0,d,floor(d/6))])
-    axs[1][0].set_xlabel("date")
-    axs[1][0].set_ylabel("total volume")
-    axs[1][0].set_title("Total volumes")
-    axs[1][0].legend()
-axs[1][1].set_axis_off()
-fig.savefig("start_data.pdf",bbox_inches="tight")
-
+        plt.semilogy(list(obj_coin.gettotal_volumes().keys()),list(obj_coin.gettotal_volumes().values()),label=obj_coin.getname())
+        plt.xticks([list(obj_coin.gettotal_volumes().keys())[i] for i in range(0,d,floor(d/12))])
+    plt.xlabel("date")
+    plt.ylabel("total volume")
+    plt.title("Total volumes")
+    plt.legend()
+    fig.savefig("Total volumes.pdf",bbox_inches="tight")
+    
+    
+    
+    
