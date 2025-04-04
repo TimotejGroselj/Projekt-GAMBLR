b7e10db03c27f59cb8bd53b4a59f4ece3f023b91
Timotej <you@example.com>
2025-04-04 19:15:40 +0200

    test_algo se m zdi de cookam tell opinion

--- testalgoritem.py
+++ testalgoritem.py
@@ -10,35 +10,53 @@ import datetime
 import pickle
 from classdat import coin
 
-def bot(podatki,money,kdm,kdv,kdp,km):
-    """
-    podatki=
-    money=mone
-    kdm=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva delta eme
-    kdv=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva delta cene
-    kdp=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva razdalja od max in min
-    km=procent kolk mone dava notr/vn glede na izračunano številko 
-
-    """
-    investicije=dict()
-    #ključ:datum
-    #vrednost:vložen denar (-float)/pobran denar ven(float) sprot updatas money
-    #če na dan nč ne nrdiš ni elementa
-    for el in podatki:
-        #podatki bojo neki nek iterator v kirm bo datum,ema,cena za cel dataset do datuma
-        date=el["date"]
-        ema=el["ema"]
-        price=el["price"]
-        past_low=min(price)
-        past_max=max(price)
-        dm="delta eme"
-        dv="delta vrednosti"
-        dp="razdalja od min in max iz preteklosti"
+class bot:
+    def __init__(self,cene,kdv,kdp,km,money=10000):
+        """
+        cene=sl. classu coin
+        money=mone to spend
+        kdv=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva delta cene
+        kdp=koef v % (vrednosti od 0 do 1) kolk na odločitev vpliva razdalja od max in min
+        km=procent kolk mone dava notr/vn glede na izračunano številko  
+        """
+        self.investicije=dict()
+        #ključ:datum
+        #vrednost:vložen denar (-float)/pobran denar ven(float) sprot updatas money
+        #če na dan nč ne nrdiš ni elementa
+        self.cene=cene
+        self.kdv=kdv
+        self.kdp=kdp
+        self.km=km
+        self.mone=money
+        self.date=list(self.cene.values().keys()).sorted()
+    def invest(self,date,amount):
+        """
+        pove kolko investirat kir dan
+        in prever če mava dost dnarja
+        če lhka še vseen investirava sam hočeva preveč investirava kkr lhka
+        pozitivn amount: izplacava nek amount
+        negativn amount: vlozva nek amount
+        """
+        if sum(list(self.investicije.value()))+amount<=self.mone:
+            self.investicije[date]=amount
+        elif sum(list(self.investicije.value()))<=self.mone:
+            self.investicije[date]=self.mone-sum(list(self.investicije.value()))
+        return None
         
+class EMA_bot(bot):
+    """
+    bot, ki simulira obnašanje eme 
+    """        
+    def __init__(self, cene, kdv, kdp, km, kdm, money=10000):
+        super().__init__(cene, kdv, kdp, km, money)
+        self.kdm=kdm
+    def calculato_faze(self,do_dneva):
+        """
+        do dneva je str oblike '%d-%m-%Y', ki pove do katerega datuma upošteva podatke
+        ter vrne le magič number, ki bo pol odločla kaj se zgodi investiciji
+        """
         
         
     
-    
-    return None
 
 
